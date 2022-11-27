e463fb36fff5d089f73d6bb50d47c21ff9c53adc
Lucas Sílva <lucas.silva_1994@outlook.com>
2022-11-26 21:44:28 -0300

    Operações matemáticas

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
-  $nome = "Lucas";
-  $idade = 28;
-  print "Meu nome é {$nome}, e tenho {$idade} anos de idade.";
+  $valor01 = 3;
+  $valor02 = 2;
+  $soma = $valor01 + $valor02;
+  $subt = $valor01 - $valor02;
+  $mult = $valor01 * $valor02;
+  $div =  $valor01 / $valor02;
+  $restoDaDivisao = $valor01 % $valor02;
+
+  print "A soma dos valores {$valor01} e {$valor02} é: {$soma}.<br/>";
+  print "A substração dos valores {$valor01} e {$valor02} é: {$subt}.<br/>";
+  print "A multiplicação dos valores {$valor01} e {$valor02} é: {$mult}.<br/>";
+  print "A divisão dos valores {$valor01} e {$valor02} é: {$div}.<br/>";
+  print "O resto da divisão dos valores {$valor01} e {$valor02} é: {$restoDaDivisao}.<br/>";
+  
