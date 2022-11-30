fbaeb806a538c1571b93e44e7877e7080384c7c5
Lucas Sílva <lucas.silva_1994@outlook.com>
2022-11-30 20:57:21 -0300

    Operadores lógicos e condicionais

--- index.php
+++ index.php
@@ -1,15 +1,42 @@
-<?php
-  $valor01 = 3;
-  $valor02 = 2;
-  $soma = $valor01 + $valor02;
-  $subt = $valor01 - $valor02;
-  $mult = $valor01 * $valor02;
-  $div =  $valor01 / $valor02;
-  $restoDaDivisao = $valor01 % $valor02;
-
-  print "A soma dos valores {$valor01} e {$valor02} é: {$soma}.<br/>";
-  print "A substração dos valores {$valor01} e {$valor02} é: {$subt}.<br/>";
-  print "A multiplicação dos valores {$valor01} e {$valor02} é: {$mult}.<br/>";
-  print "A divisão dos valores {$valor01} e {$valor02} é: {$div}.<br/>";
-  print "O resto da divisão dos valores {$valor01} e {$valor02} é: {$restoDaDivisao}.<br/>";
-  
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <title>Revisando PHP</title>
+</head>
+<body>
+  <div class="container mt-3">
+    <form action="" method="POST">
+      <div class="row">
+        <div class="col-lg-7">
+          <input type="text" name="nome" id="nome" required class="form-control" />
+        </div>
+        <div class="col-lg-2">
+          <input type="number" name="idade" id="idade" required class="form-control" />
+        </div>
+        <div class="col-lg-3 d-grid">
+          <input type="submit" value="Enviar" name="enviar" class="btn btn-primary d-block" />
+        </div>
+      </div>
+    </form>
+    <?php
+          if (isset($_POST['enviar'])) {
+            $idade = $_POST['idade'];
+            $nome = $_POST['nome'];
+            if ($idade >= 18 && $idade <= 100) {
+              print "Olá <strong>$nome</strong>, você tem $idade anos e você já é maior de idade.";
+              return;
+            }
+            if($idade <=0 OR $idade > 100){
+              print "Olá <strong>$nome</strong>, informe uma idade válida.";
+              return;
+            } 
+              print "Olá <strong>$nome</strong>, você tem $idade anos e você ainda não é maior de idade.";
+          }
+    ?>
+  </div>
+</body>
+</html>
