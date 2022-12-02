01727483f3f27f91b741537e558f29da017b10fe
Lucas Sílva <lucas.silva_1994@outlook.com>
2022-12-01 21:45:51 -0300

    Implementando tabuada básica de multiplicação

---
+++ tabuada.php
@@ -0,0 +1,6 @@
+<?php
+    $multiplicador = 8;
+    for($contador=1; $contador<=10;$contador++){
+        $resultado = $multiplicador*$contador;
+        print "$multiplicador x $contador = $resultado <br/>";
+    }
