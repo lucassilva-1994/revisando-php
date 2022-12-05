ff7f1819705482bc2ade0f58b87c1939ad11905c
Lucas Sílva <lucas.silva_1994@outlook.com>
2022-12-05 20:22:05 -0300

    Arrays associativos (foreach)

--- arrays-associativo.php
+++ arrays-associativo.php
@@ -1,10 +1,11 @@
 <?php
-    $conta1 = ["titular" => "Pessoa 01","saldo" => 1200.00];
-    $conta2 = ["titular" => "Pessoa 02","saldo" => 1300.00];
-    $conta3 = ["titular" => "Pessoa 03","saldo" => 1250.00];
+    $conta1 = ["titular" => "Pessoa 01","saldo" => 1200.91];
+    $conta2 = ["titular" => "Pessoa 02","saldo" => 1300.09];
+    $conta3 = ["titular" => "Pessoa 03","saldo" => 1250.56];
+    $conta4 = ["titular" => "Pessoa 04","saldo" => 1500.01];
     $prefixo = "R$ ";
-    $contasCorrentes = [$conta1,$conta2,$conta3];
-    for ($i=0;$i<count($contasCorrentes);$i++){
-        print $contasCorrentes[$i]['titular']."<br/>";
-        print $prefixo.$contasCorrentes[$i]['saldo']."<hr/>";
+    $contasCorrentes = [$conta1,$conta2,$conta3,$conta4];
+    foreach($contasCorrentes as $conta){
+        print $conta["titular"]."<br/>";
+        print $prefixo.$conta["saldo"]."<hr/>";
     }
