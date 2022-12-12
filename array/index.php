b1121483a33fbf6bfa6deaa92c565cd4d08cb8ba
Lucas Sílva <lucas.silva_1994@outlook.com>
2022-12-11 22:08:33 -0300

    Ordenando arrays multidimensional com usort

--- array/index.php
+++ array/index.php
@@ -1,6 +1,24 @@
 <?php
-    $array = [10,9,22,31,50,11,6];
-    sort($array);
-    foreach($array as $valor){
-        print "$valor<br/>";
+    $notas = [
+        [ "aluno" => "Alice", "nota" => 8],
+        ["aluno" => "Fabiano", "nota" => 7.5],
+        ["aluno" => "Rafaela","nota" => 9],
+        ["aluno" => "Lorena", "nota" => 6]
+    ];
+
+    function ordenaNotas(array $nota1, array $nota2): int{
+        if($nota1['nota'] > $nota2['nota']){
+            return -1;
+        }
+
+        if($nota2['nota'] > $nota1['nota']){
+            return 1;
+        }
+
+        return 0;
     }
+
+    usort($notas, 'ordenaNotas');
+    print "<pre>";
+    var_dump($notas);
+    print "</pre>";
