006d0eb2966c5583dbc5b70688d41be84db22ae3
Lucas Sílva <lucas.silva_1994@outlook.com>
2022-12-15 11:21:27 -0300

    Adicionando elementos na lista (Com array_push, [] e array_unshift) e removendo elementos da lista (Com array_shift e array_pop)

--- array/notas.php
+++ array/notas.php
@@ -17,6 +17,19 @@ $novosalunos = [
 ];
 
 $alunos2022 = [...$alunos2021, "Lucas Sílva",...$novosalunos];
+//Adicionando elementos no final da lista
+array_push($alunos2022, "Antônio", "Leonardo", "Geslaine", "Claúdia");
+//Outra maneira de adicionar elementos no final da lista
+$alunos2022[] = "Leonardo";
+//Adicionando elementos no inicio da lista
+array_unshift($alunos2022, "Stephanie", "Rafaela");
+
+
+//Removendo primeiro elemento da lista
+array_shift($alunos2022);
+//Removendo último elemento da lista
+array_pop($alunos2022);
+
 print "<pre>";
 var_dump($alunos2022);
 print "</pre>";
