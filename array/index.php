c5fa284f3c4b00aefc5189356c930fdebdccbed1
Lucas Sílva <lucas.silva_1994@outlook.com>
2022-12-15 19:08:29 -0300

    list e loop (Usando o foreach)

--- array/index.php
+++ array/index.php
@@ -6,11 +6,6 @@
         ["aluno" => "Lorena", "nota" => 6]
     ];
 
-    function ordenaNotas(array $nota1, array $nota2): int{
-        return $nota2['nota'] <=> $nota1['nota'];
+    foreach($notas as ["aluno"=> $aluno, "nota" => $nota]){
+        print "Aluno: $aluno &nbsp; Nota: $nota <br/>";
     }
-
-    usort($notas, 'ordenaNotas');
-    print "<pre>";
-    var_dump($notas);
-    print "</pre>";
